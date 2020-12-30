b7c44bd21d443f22681f67a9a0881c4dcd396901
attepee <atte.pekkarinen@outlook.com>
2020-12-30 17:39:11 +0200

    Add option to include a random symbol

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
- var wordlist = null;
+var wordlist = null;
+var hasSymbol = false;
+var forceSymbol = false;
 
 function main() {
     if (wordlist != null)
@@ -26,32 +28,37 @@ function generatePassPhrase() {
     var pass = "";
     var wordCount = document.getElementById("wordCount").value;
     var wordSeparator = document.getElementById("wordSeparator").value;
-    var randCap = document.getElementById("randCap").checked
-    var cap = document.getElementById("cap").checked
+    var randCap = document.getElementById("randCap").checked;
+    var cap = document.getElementById("cap").checked;
+    var sym = document.getElementById("sym").checked;
+    hasSymbol = false;
+    forceSymbol = false;
 
     for (var i = 0; i < wordCount; i++) {
-        if (i < wordCount-1) {
-            if (cap == true)
-                pass += capitalize(getWord() + wordSeparator);
-            else
-                pass += getWord() + wordSeparator;
-        }
-        else {
-            if (cap == true)
-                pass += capitalize(getWord());
-            else
-                pass += getWord();
-        }
+        if (i+1 == wordCount && !hasSymbol)
+            forceSymbol = true;
+        if (cap)
+            pass += capitalize(getWord(sym));
+        else if (randCap)
+            pass += randomCapitalize(getWord(sym));
+        else
+            pass += getWord(sym);
+        if (i < wordCount-1)
+            pass += wordSeparator;
     }
-
-    if (randCap == true)
-        pass = randomCapitalize(pass);
 
     document.getElementById("passphrase").innerHTML = pass;
 }
 
-function getWord() {
-    return wordlist[getRandom(0, 94109)].childNodes[0].nodeValue
+function getWord(sym) {
+    var r = Math.round(Math.random());
+
+    if (sym && !hasSymbol && r || sym && forceSymbol) {
+        hasSymbol = true;
+        return addSymbol(wordlist[getRandom(0, 94109)].childNodes[0].nodeValue);
+    }
+    else
+        return wordlist[getRandom(0, 94109)].childNodes[0].nodeValue;
 }
 
 function getRandom(min, max) {
@@ -59,12 +66,18 @@ function getRandom(min, max) {
 }
 
 function capitalize(str){
-    return str.charAt(0).toUpperCase() + str.slice(1)
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 function randomCapitalize(str) {
     var capStr = str.split("").map(function(c) {
         return c[Math.round(Math.random())?"toUpperCase":"toLowerCase"]();
     }).join("");
+
     return capStr;
+}
+
+function addSymbol(str) {
+    var sym = "!#%&?@";
+    return str += sym[getRandom(0, sym.length)];
 }
