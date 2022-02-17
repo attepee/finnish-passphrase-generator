cbd1d48e2ac09f4dc81ec5ec9554e50523ffc710
L9406 <l9406@student.jamk.fi>
2022-02-17 13:17:17 +0200

    Add option to filter words with letters å, ö or å

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
-var wordlist = null;
+var wordlist = [];
+var filteredWordlist = [];
 
 function loadXMLDoc() {
     var xmlHttpReq = new XMLHttpRequest();
     
     xmlHttpReq.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var xml = this.responseXML;
-            wordlist = xml.getElementsByTagName("s");
+            var xml = this.responseXML.getElementsByTagName("s");
+            for(var i = 0; i < xml.length; i++) {
+                wordlist.push(xml[i].childNodes[0].nodeValue);
+            }
         }
     };
     xmlHttpReq.open("GET", "wordlist/kotus-sanalista_v1.xml", true);
@@ -20,11 +23,18 @@ function generatePassPhrase() {
     var capitals = document.getElementById("capType").value;
     var sym = document.getElementById("sym").checked;
     var num = document.getElementById("num").checked;
+    var noSpecialLetters = document.getElementById("noSpecialLetters").checked;
     var hasSymbol = false;
     var hasNumber = false;
 
+    if (noSpecialLetters && filteredWordlist.length == 0) {
+        wordlist.forEach(word => {
+            /^[a-z]+$/g.test(word) ? filteredWordlist.push(word) : false ;
+        });
+    }
+
     for (var i = 1; i <= wordCount; i++) {
-        pass += getWord(capitals);
+        pass += getWord(capitals, noSpecialLetters);
 
         if (sym && !hasSymbol &&getRandom(0, 2) == 1) {
             pass += addSymbol();
@@ -46,8 +56,11 @@ function generatePassPhrase() {
     document.getElementById("passphrase").value = pass;
 }
 
-function getWord(capitals) {
-    var str = wordlist[getRandom(0, 94109)].childNodes[0].nodeValue;
+function getWord(capitals, noSpecialLetters) {
+    if(noSpecialLetters)
+        var str = filteredWordlist[getRandom(0, filteredWordlist.length - 1)];
+    else
+        var str = wordlist[getRandom(0, wordlist.length - 1)];
     
     if (capitals == "noCap")
         return str;
